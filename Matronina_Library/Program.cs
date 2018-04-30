4e8d4c66cc51838ee27d76ff93176904be7e0c97
lissychu <lissychu@gmail.com>
2018-04-30 12:34:44 +0300

    Изменение текста

--- Matronina_Library/Program.cs
+++ Matronina_Library/Program.cs
@@ -9,7 +9,7 @@ namespace Matronina_Library
     static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        /// Главная точка входа для приложения!!!
         /// </summary>
         [STAThread]
         static void Main()
